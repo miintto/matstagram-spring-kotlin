4a638b5465ad5fb3063e99f78ae0082fd75b2fbb
miintto <minjp93@gmail.com>
2023-05-24 16:26:46 +0900

    feat: 로깅 모듈 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter")
 
+	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 	implementation("org.postgresql:postgresql")
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/com/miintto/matstagram/common/exception/APIExceptionHandler.kt
+++ src/main/kotlin/com/miintto/matstagram/common/exception/APIExceptionHandler.kt
@@ -2,18 +2,23 @@ package com.miintto.matstagram.common.exception
 
 import com.miintto.matstagram.common.response.APIResponse
 import com.miintto.matstagram.common.response.code.Http5xx
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+
+private val logger = KotlinLogging.logger {}
 
 @ControllerAdvice
 class APIExceptionHandler {
     @ExceptionHandler(value = [Exception::class])
     fun exception(e: Exception) : APIResponse {
+        logger.error(e.message, e)
         return APIResponse(Http5xx.SERVER_ERROR)
     }
 
     @ExceptionHandler(value = [APIException::class])
     fun exception(e: APIException) : APIResponse {
+        logger.error(e.toString())
         return APIResponse(e.responseFormat, e.data)
     }
 }
