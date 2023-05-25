3f4cff3c759b6ba1c8488439de8227da651271ea
miintto <minjp93@gmail.com>
2023-05-26 00:00:52 +0900

    feat: 라이브러리 버전 fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,13 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter")
 
 	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-	implementation("org.postgresql:postgresql")
+	implementation("org.postgresql:postgresql:42.5.4")
 
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
