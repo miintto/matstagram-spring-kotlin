996602319d660904e29d741f8c3c5f5656dfb381
miintto <minjp93@gmail.com>
2023-09-24 15:20:21 +0900

    feat: 사용자 정보 조회시 비밀번호 제거

--- src/main/kotlin/com/miintto/matstagram/domain/repository/AuthUserRepository.kt
+++ src/main/kotlin/com/miintto/matstagram/domain/repository/AuthUserRepository.kt
@@ -1,11 +1,16 @@
 package com.miintto.matstagram.domain.repository
 
 import com.miintto.matstagram.domain.AuthUser
+import com.miintto.matstagram.domain.projection.AuthUserSummary
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface AuthUserRepository : JpaRepository<AuthUser, Long> {
+
+    @Query("SELECT u FROM AuthUser u WHERE u.id = :id")
+    fun findProjectedById(id: Long): AuthUserSummary?
 
     fun findByUserEmail(userEmail: String): AuthUser?
 
--- src/main/kotlin/com/miintto/matstagram/service/UserService.kt
+++ src/main/kotlin/com/miintto/matstagram/service/UserService.kt
@@ -1,11 +1,10 @@
 package com.miintto.matstagram.service
 
-import com.miintto.matstagram.domain.AuthUser
 import com.miintto.matstagram.domain.repository.AuthUserRepository
 import com.miintto.matstagram.common.exception.ApiException
 import com.miintto.matstagram.common.response.code.Http4xx
+import com.miintto.matstagram.domain.projection.AuthUserSummary
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,8 +13,8 @@ class UserService {
     @Autowired
     lateinit var authUserRepository: AuthUserRepository
 
-    fun getUserById(userId: Long): AuthUser {
-        return authUserRepository.findByIdOrNull(userId)
+    fun getUserById(userId: Long): AuthUserSummary {
+        return authUserRepository.findProjectedById(userId)
             ?: throw ApiException(Http4xx.USER_NOT_FOUND)
     }
 }
